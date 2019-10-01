3811ee9e3a1e2def91fd16db9c332332a9f0608c
Toki <actocchi@gmail.com>
2019-09-30 22:40:16 -0700

    using correct url for 5day forcast this time

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ $("#search-Btn").on("click", function(){
      let city = $(".form-control").val();
     console.log(city);
     
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=0c6a6a50580bec02658ec3ed509bbbfa";
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=0c6a6a50580bec02658ec3ed509bbbfa";
     
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        // $("#city-name").text(response.name);
-        // $("#wind").text(response.wind.speed);
+        $("#city-name").text(response.city.name + +response.list[0].dt + " " + response.list[0].weather[0].icon);
+        $("#wind").text("Wind Speed: " + response.list[0].wind.speed +"MPH");
+        $("#humidity").text("Humidity: " + response.list[0].main.humidity + "%");
     });
     
 });

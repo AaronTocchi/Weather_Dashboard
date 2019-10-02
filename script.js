0da923b871d81384690adc6fda7e69c1c607e11a
Toki <actocchi@gmail.com>
2019-10-02 10:35:04 -0700

    cleaned up javascript code notes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+'use strict'
 
 // id/api key for open weather map
 let appKey = "0c6a6a50580bec02658ec3ed509bbbfa";
@@ -6,7 +6,9 @@ let city;
 let lat;
 let lon;
 let date;
+let newButton;
 
+// function to pull api for dayweather and set text.
 function getDayWeather(cb) {
 
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + appKey;
@@ -33,59 +35,59 @@ function getDayWeather(cb) {
         $("#city-name").append(icon);
 
         cb();
-        
-    });
-    
-};
 
+    });
+
+};
+// function to get the api for 5day forcast and display to the 5 columns that I made
 function getForcast() {
-    
+
     let forcastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + appKey;
-    
+
     $.ajax({
         url: forcastURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
         // day 1 forcast (i know theres a cleaner way but for now this is how it is)
-        // TODO: ADD ICONS TO EACH DAY 
+        // TODO: 
         $("#date1").text(response.list[0].dt_txt.substr(0, 10));
         $("#humidity1").text("Humidity: " + response.list[0].main.humidity + "%");
         $("#temperature1").text("Temperature: " + response.list[0].main.temp + "°f");
-        $("#icon1").attr("src","https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+        $("#icon1").attr("src", "https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
         // day 2
         $("#date2").text(response.list[8].dt_txt.substr(0, 10));
         $("#humidity2").text("Humidity: " + response.list[8].main.humidity + "%");
         $("#temperature2").text("Temperature: " + response.list[8].main.temp + "°f");
-        $("#icon2").attr("src","https://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png");
+        $("#icon2").attr("src", "https://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png");
         // day 3
         $("#date3").text(response.list[16].dt_txt.substr(0, 10));
         $("#humidity3").text("Humidity: " + response.list[16].main.humidity + "%");
         $("#temperature3").text("Temperature: " + response.list[16].main.temp + "°f");
-        $("#icon3").attr("src","https://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png");
+        $("#icon3").attr("src", "https://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png");
         // day 4
         $("#date4").text(response.list[24].dt_txt.substr(0, 10));
         $("#humidity4").text("Humidity: " + response.list[24].main.humidity + "%");
         $("#temperature4").text("Temperature: " + response.list[24].main.temp + "°f");
-        $("#icon4").attr("src","https://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png");
+        $("#icon4").attr("src", "https://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png");
         // day 5
         $("#date5").text(response.list[32].dt_txt.substr(0, 10));
         $("#humidity5").text("Humidity: " + response.list[32].main.humidity + "%");
         $("#temperature5").text("Temperature: " + response.list[32].main.temp + "°f");
-        $("#icon5").attr("src","https://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png");
+        $("#icon5").attr("src", "https://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png");
         // let icon =  $("<img>"); 
         // icon.attr( "src", "https://openweathermap.org/img/w/" +  response.list[0].weather[0].id +"@2x.png");
         // $("#city-name").append(icon);
         //  console.log(icon);
 
     });
-    
-};
 
+};
+// function to get the api for the UV index and display it
 function getUV() {
-    console.log(lat,lon);
+    console.log(lat, lon);
     let uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + appKey + "&lat=" + lat + "&lon=" + lon;
-    
+
     $.ajax({
         url: uvURL,
         method: "GET"
@@ -94,94 +96,28 @@ function getUV() {
         $("#uv-index").text("UV Index: " + response.value);
     });
 };
-
+// function to make a new button in the list of cities that you've serached
 function newcitybtn() {
-    
+
     newButton = $("<button class='newCity'>").text(city);
     $("#city-list").append(newButton);
     $(".form-control").val("");
 };
-
+// event to search for cities on click
 $("#search-Btn").on("click", function () {
     event.preventDefault();
-    
+
     city = $(".form-control").val();
     getDayWeather(getUV);
     getForcast();
     // getUV();
     newcitybtn();
-    
-    
 
-    // ajax function for day of weather
-    // $.ajax({
-    //     url: weatherURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(response);
-    //     $("#city-name").text(response.name + " ");
-    //     $("#wind").text("Wind Speed: " + response.wind.speed + "MPH");
-    //     $("#temperature").text("Temperature: " + response.main.temp + "°f");
-    //     $("#humidity").text("Humidity: " + response.main.humidity + "%");
-    //     let lat = response.coord.lat;
-    //     let lon = response.coord.lon;
-    //     console.log(lat, lon);
-    //     // icon for weather display
-    //     let icon = $("<img>");
-    //     icon.attr("src", "https://openweathermap.org/img/w/${" + response.weather[0].icon + "}@2x.png");
-    //     $("#city-name").append(icon);
-    // console.log(icon);
-    //  ajax inside of ajax. to try to maybe get UV cant stop me now....
-    // let uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + appKey + "&lat=" + lat + "&lon=" + lon;
-    // $.ajax({
-    //     url: uvURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(response);
-
-
-
-    // });
-    // });
-    // ajax function for 5 day forcast
-    // $.ajax({
-    //     url: forcastURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(response);
-    //     // day 1 forcast (i know theres a cleaner way but for now this is how it is)
-    //     $("#date1").text(response.list[0].dt_txt.substr(0, 10));
-    //     $("#humidity1").text("Humidity: " + response.list[0].main.humidity + "%");
-    //     $("#temperature1").text("Temperature: " + response.list[0].main.temp + "°f");
-    //     // day 2
-    //     $("#date2").text(response.list[8].dt_txt.substr(0, 10));
-    //     $("#humidity2").text("Humidity: " + response.list[8].main.humidity + "%");
-    //     $("#temperature2").text("Temperature: " + response.list[8].main.temp + "°f");
-    //     // day 3
-    //     $("#date3").text(response.list[16].dt_txt.substr(0, 10));
-    //     $("#humidity3").text("Humidity: " + response.list[16].main.humidity + "%");
-    //     $("#temperature3").text("Temperature: " + response.list[16].main.temp + "°f");
-    //     // day 4
-    //     $("#date4").text(response.list[24].dt_txt.substr(0, 10));
-    //     $("#humidity4").text("Humidity: " + response.list[24].main.humidity + "%");
-    //     $("#temperature4").text("Temperature: " + response.list[24].main.temp + "°f");
-    //     // day 5
-    //     $("#date5").text(response.list[32].dt_txt.substr(0, 10));
-    //     $("#humidity5").text("Humidity: " + response.list[32].main.humidity + "%");
-    //     $("#temperature5").text("Temperature: " + response.list[32].main.temp + "°f");
-
-    //     // let icon =  $("<img>"); 
-    //     // icon.attr( "src", "https://openweathermap.org/img/w/" +  response.list[0].weather[0].id +"@2x.png");
-    //     // $("#city-name").append(icon);
-    //     //  console.log(icon);
-
-    // });
-
-    
 });
 
 
-$("#city-list").on("click", '.newCity', function() {
+// event for clicking on the new children that are creadted in city list
+$("#city-list").on("click", '.newCity', function () {
     event.preventDefault();
     console.log("hello");
     let clicked = $(this);
@@ -191,7 +127,7 @@ $("#city-list").on("click", '.newCity', function() {
     getDayWeather(getUV);
     getForcast();
 
-    
+
 });
 
-// my second function look how lil it is... the other one should be more like this. Also if you read this, thank you. You care and you're wearing a cute hat today
+    // look how neat my code is now :). Also if you read this, thank you. You care and you're wearing a cute hat today
